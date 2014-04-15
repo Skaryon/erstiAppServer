408c6842bdd06b7653fe35474c1ff05bfffc61cc
Skaryon <michael.klein.rulez@googlemail.com>
2014-04-15 10:53:01 +0200

    Parsen von URL Parametern eingebaut

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var server = (function () {
     //private
     var self = this;
     var http = require('http'),
+        url = require('url'),
         cheerio = require('cheerio'),
         fs = require('fs'),
         $ = null;
@@ -15,9 +16,17 @@ var server = (function () {
             //create server
             console.log("starting server... /");
             http.createServer(function (request, response) {
+                //get url get vars
+                var url_parts = url.parse(request.url, true);
+                var query = url_parts.query;
+                if (Object.keys(query).length > 0)
+                    console.log("the following url parameters were send: /")
+                for (varName in query) {
+                    console.log(varName + "=" + query[varName] + "/");
+                }
                 //the following code will create a simple webserver which renders websites
-                var file = __dirname + request.url;
-                if (request.url === "/") file = __dirname + "/index.html";
+                var file = __dirname + url_parts.pathname;
+                if (url_parts.pathname === "/") file = __dirname + "/index.html";
                 console.log("incoming request for:  " + file + "/");
                 console.log("checking if '" + file + "' exists... /")
                 fs.exists(file, function (exists) {
@@ -31,12 +40,14 @@ var server = (function () {
                         });
                     } else {
                         if (exists) {
+                            console.log("file exists, loading it... /");
                             file = fs.readFileSync(file, 'utf8');
                         }
                         else {
+                            console.log("file does not exist, loading 404... /");
                             file = fs.readFileSync(__dirname + "/404.html", 'utf8');
                         }
-
+                        console.log("outputting file /");
                         //output html
                         response.writeHeader(200, {
                             "Content-Type": "text/html"
